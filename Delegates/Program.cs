8ec9b7f4982465d06074da3a626fbc47505b84d5
joaoveasey <jovepedro@gmail.com>
2024-02-13 12:48:44 -0300

    #

---
+++ Delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿// DELEGATE PREDICATE: método que recebe um argumento de tipo T e retorna um bool.
+
+// DELEGATE ACTION: método que não retorna um valor, as pode receber até 16 args de entrada, útil para representar um método que executa uma ação e não retorna nada
+
+// DELEGATE FUNC: recebe de 1 a 16 args do tipo T de entrada e retorna um valor do tipo TResult, útil para representar um método que executa uma operação e retorna um resultado.
+
+
+//---------- usando um deleagte PREDICATE -----------
+//Console.WriteLine("Informe um número inteiro");
+//int numero = Convert.ToInt32(Console.ReadLine());
+
+//Predicate<int> delegatePar = x => x % 2 == 0; 
+
+//if (delegatePar(numero))
+//{
+//    Console.WriteLine("PAR");
+//}
+//else
+//{
+//    Console.WriteLine("IMPAR");
+//}
+
+//Console.ReadKey ();
+
+//---------- usando um delegate ACTION -----------
+
+//Console.WriteLine("Informe um numero inteiro: ");
+//int numero = Convert.ToInt16(Console.ReadLine());
+
+//Action<int> dobra = x => Console.WriteLine(x * 2);
+//dobra(numero);
+
+//Console.ReadKey();
+
+//---------- usando um delegate FUNCTION -----------
+
+double numero = Convert.ToDouble(Console.ReadLine());
+
+Func<double,double> raizQuadrada = x => Math.Sqrt(x);
+
+double resultado = raizQuadrada(numero);
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
---
+++ MetodosAnonimos/Program.cs
@@ -0,0 +1,45 @@
+﻿// métodos anônimos com delegate
+
+//Imprimir imprimir = delegate (int valor)
+//{
+//    Console.WriteLine($"Método anônimo. Valor: {valor}");
+//};
+
+//imprimir(100);
+
+//Console.ReadKey();
+
+//public delegate void Imprimir(int valor);
+
+
+//----------------- OUTRO EXEMPLO -----------------
+
+List<string> nomes = new List<string>();
+nomes.Add("Maria");
+nomes.Add("Miriam");
+nomes.Add("Paulo");
+nomes.Add("João");
+nomes.Add("Carlos");
+
+//string resultado = nomes.Find(VerificaNomeNaLista);
+
+string resultadoComMetodoAnonimo = nomes.Find(delegate (string nome)
+{
+    return nome.Equals("Paulo");
+});
+
+//mesmo codigo acima porém usando expressão LAMBDA =>
+
+string resultadoComLambda = nomes.Find(nome => nome.Equals("Paulo"));
+
+//resultados
+
+Console.WriteLine(resultadoComMetodoAnonimo);
+Console.WriteLine(resultadoComLambda);
+
+Console.ReadKey();
+
+//static bool VerificaNomeNaLista(string nome)
+//{
+//    return nome.Equals("Paulo");
+//}
